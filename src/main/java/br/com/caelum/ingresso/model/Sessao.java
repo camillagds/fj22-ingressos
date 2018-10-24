ee7c9333c3f7c359a3463c13a969166e09c938e9
camillagds <milla@silverwolf.com.br>
2018-10-24 20:48:01 -0300

    Implementando ação de criar sessão

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,14 +1,25 @@
 package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
-
+@Entity
 public class Sessao {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
 	
+	public Sessao(){
+		
+	}
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
 		super();
 		this.horario = horario;
@@ -20,35 +31,35 @@ public class Sessao {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
 
-	private Integer getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	private void setId(Integer id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	private LocalTime getHorario() {
+	public LocalTime getHorario() {
 		return horario;
 	}
 
-	private void setHorario(LocalTime horario) {
+	public void setHorario(LocalTime horario) {
 		this.horario = horario;
 	}
 
-	private Sala getSala() {
+	public Sala getSala() {
 		return sala;
 	}
 
-	private void setSala(Sala sala) {
+	public void setSala(Sala sala) {
 		this.sala = sala;
 	}
 
-	private Filme getFilme() {
+	public Filme getFilme() {
 		return filme;
 	}
 
-	private void setFilme(Filme filme) {
+	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
  }
